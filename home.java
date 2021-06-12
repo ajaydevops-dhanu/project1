624ee0cc7fee91840e4901a97a2bc27b3585eb16
root <root@ip-172-31-35-54.ap-south-1.compute.internal>
2021-06-12 16:06:00 +0000

    part of my new prj

---
+++ home.java
@@ -0,0 +1,7 @@
+class
+{
+public statis void main(string []args)
+{
+system.out.prinln("output coming from home.java");
+}
+}
---
+++ login.java
@@ -0,0 +1,7 @@
+class Login
+{
+public statis void main(string []args)
+{
+system.out.prinln("output coming from login.java");
+}
+}
